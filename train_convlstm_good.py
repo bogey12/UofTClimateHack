48ef1c75895e943ec6608e263a1878776c15db99
loluwot <fontisawesome@gmail.com>
2022-02-28 00:47:26 -0500

    fix

--- train_convlstm_good.py
+++ train_convlstm_good.py
@@ -29,6 +29,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Train skip conn UNet')
 parser.add_argument('--lr', required=True,
                     help='lr', type=float)
+parser.add_argument('--dropout', required=False,
+                    help='lr', type=float, default=0.1)
 parser.add_argument('--normalize', required=False,
                     help='norm', type=str, default="standardize")
 parser.add_argument('--epochs', required=False,
@@ -51,11 +53,12 @@ class TempModel(nn.Module):
         self.encoder = Encoder2(convlstm_encoder_params[0], convlstm_encoder_params[1])
         self.forecaster = Forecaster(convlstm_forecaster_params[0], convlstm_forecaster_params[1])
         self.ef = EF(self.encoder, self.forecaster)
+        self.dropout = nn.Dropout(config['dropout'])
 
     def forward(self, features):
         x = rearrange(features, 'b (c t) h w -> t b c h w', c=1)
         x = self.dropout(x)
-        x = self.rencoding(x)
+        x = self.ef(x)
         x = rearrange(x, 't b c h w -> b (t c) h w')
         return x
 
@@ -104,6 +107,7 @@ if __name__ == '__main__':
         "output_std": 0,
         "inputs":args['inputs'],
         "outputs":args['outputs'],
+        "dropout": args['dropout']
     }
     
     training_model = PredictionTrainer(config, model=TempModel, device=device, convert=False)
